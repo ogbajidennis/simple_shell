69fee9971dda992df7f166f0b59d3f574709164c
Agbo Amarachukwu Emmanuella <agboella20@gmail.com>
2023-04-19 03:14:43 +0100

    update main.h

--- main.h
+++ main.h
@@ -2,9 +2,17 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <stdlib.h>
+
+int _strlen(char *s);
+
+void execmnd(char **argv);
+
+char *get_path_location(char *command)
 
 #endif
